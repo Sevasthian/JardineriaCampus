04c9b9adeb9e3a825df4afb2864e601bab8fcd50
RoyverSevasthianVerdugoRojas <160695272+Sevasthian@users.noreply.github.com>
2024-03-06 15:19:44 -0500

    terminando ne review

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+# from tabulate import tabulate
 import modules.getClients as cliente
 import modules.getOficina as oficina
 import modules.getEmpleado as empleado
-from tabulate import tabulate
  
 
  
 
-print(tabulate(empleado.getAllNombreDelPuestoApellidosEmailJefe()))
+print(cliente.getAllNombreDeClientesEspañoles())
