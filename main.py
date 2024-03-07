8e340a637cfdb86e6622661e26513b4b6ae1048a
RoyverSevasthianVerdugoRojas <160695272+Sevasthian@users.noreply.github.com>
2024-03-07 09:10:07 -0500

    incompleto

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-# from tabulate import tabulate
 import modules.getClients as cliente
 import modules.getOficina as oficina
 import modules.getEmpleado as empleado
+import modules.getPedido as pedido
+from tabulate import tabulate
  
 
  
 
-print(cliente.getAllNombreDeClientesEspañoles())
+print(tabulate(pedido.getAllPedidosRechazadosEn2009()))
