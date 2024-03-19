12092a10acc2fd41495633c9b3db87030d6348ea
Sevasthian <160695272+Sevasthian@users.noreply.github.com>
2024-03-19 09:16:34 -0500

    actualizacion

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ import modules.getEmpleado as empleado
 import modules.getPedido as pedido
 import modules.getPagos as pago
 import modules.getProducto as producto
-import modules.postProducto as post
+import modules.postProducto as postpro
+import modules.getGamas as gama
 from tabulate import tabulate
 import modules.postEmpleado as PEmp
+import modules.postPedido as PstPedi
+import modules.postPagos as pstpag
+import modules.postOficina as pstOfi
+import modules.postGamas as pstGam
+import modules.postCliente as pstCli
 import requests
 
 import os
@@ -22,7 +28,7 @@ import os
 #           ''')
 # print(dir())
 
-# # # print(pedido. getAllPedidosEntregadosEnero())
+# # print(pedido. getAllPedidosEntregadosEnero())
 # def menu():
 #     contador = 1
 #     print("Menu principal")
@@ -30,152 +36,274 @@ import os
 #         if nombre.startswith("modulo"):
 #             modulo = getattr(objeto, "__name__", None)
 #         if(modulo != "modulo"):
-#         
+        
 # }
 
 #    file = modulo.split("get")[- 1]
 #             print(f'''{contador}.{file}''')
 # #             contador += 1
 # """ 
-# def menuEmpleado():
-#       os.system("clear")
-#       while True:
-#         print('''
+def menuCliente():
+      while True:
+            os.system("clear")
+            print('''
+bienvenido al menu de clientes
+                  0.Atras
+                  1. Reportes de los clientes
+                  2. Guardas algun cliente
+                  3. Cerrar el programa''')
+            try:
+                      opciones = int(input("Ingrese el numero de la opcion deceada: "))
+                      if opciones == 0:
+                              break
+                      elif opciones == 1:
+                              cliente.menu()
+                      elif opciones == 2:
+                              pstCli.menu()
+                      elif opciones == 3:
+                            exit()
+            except ValueError as error:
+                    input("Oprima enter para continuar con el programa")
+                    print(error)
+                    
+            except KeyboardInterrupt as error:
+                    input("Oprima enter para continuar con el programa")
+                    print(error)     
+                  
+def menuEmpleado():
+      while True:
+        os.system("clear")
+        print('''
 
-#     ____  _                            _     __               __                                  
-#    / __ )(_)__  ____ _   _____  ____  (_)___/ /___     ____ _/ /                                  
-#   / __  / / _ \/ __ \ | / / _ \/ __ \/ / __  / __ \   / __ `/ /                                   
-#  / /_/ / /  __/ / / / |/ /  __/ / / / / /_/ / /_/ /  / /_/ / /                                    
-# /_____/_/\___/_/ /_/|___/\___/_/ /_/_/\__,_/\____/   \__,_/_/        __               __          
-#    ____ ___  ___  ____  __/_/_   ____/ /__     ___  ____ ___  ____  / /__  ____ _____/ /___  _____
-#   / __ `__ \/ _ \/ __ \/ / / /  / __  / _ \   / _ \/ __ `__ \/ __ \/ / _ \/ __ `/ __  / __ \/ ___/
-#  / / / / / /  __/ / / / /_/ /  / /_/ /  __/  /  __/ / / / / / /_/ / /  __/ /_/ / /_/ / /_/ (__  ) 
-# /_/ /_/ /_/\___/_/ /_/\__,_/   \__,_/\___/   \___/_/ /_/ /_/ .___/_/\___/\__,_/\__,_/\____/____/  
-#                                                           /_/                                     
-#         0. Atras
-#               1.Reportes de los empleados
-#               2.Guardar alguna empleado
-#         3. Cerrar el programa
+    ____  _                            _     __               __                                  
+   / __ )(_)__  ____ _   _____  ____  (_)___/ /___     ____ _/ /                                  
+  / __  / / _ \/ __ \ | / / _ \/ __ \/ / __  / __ \   / __ `/ /                                   
+ / /_/ / /  __/ / / / |/ /  __/ / / / / /_/ / /_/ /  / /_/ / /                                    
+/_____/_/\___/_/ /_/|___/\___/_/ /_/_/\__,_/\____/   \__,_/_/        __               __          
+   ____ ___  ___  ____  __/_/_   ____/ /__     ___  ____ ___  ____  / /__  ____ _____/ /___  _____
+  / __ `__ \/ _ \/ __ \/ / / /  / __  / _ \   / _ \/ __ `__ \/ __ \/ / _ \/ __ `/ __  / __ \/ ___/
+ / / / / / /  __/ / / / /_/ /  / /_/ /  __/  /  __/ / / / / / /_/ / /  __/ /_/ / /_/ / /_/ (__  ) 
+/_/ /_/ /_/\___/_/ /_/\__,_/   \__,_/\___/   \___/_/ /_/ /_/ .___/_/\___/\__,_/\__,_/\____/____/  
+                                                          /_/                                     
+        0. Atras
+              1.Reportes de los empleados
+              2.Guardar alguna empleado
+        3. Cerrar el programa
 
-# ''')
-#         try:
-#                 opciones = int(input("Ingrese el numero de la opcion deceada: "))
-#                 if opciones == 0:
-#                         break
-#                 elif opciones == 1:
-#                         cliente.menu()
-#                 elif opciones == 2:
-#                         PEmp.menu()
-#                 elif opciones == 3:
-#                       exit()
-#         except ValueError as error:
-#               print("Oprima enter para continuar con el programa", error)
-#         except KeyboardInterrupt as error:
-#               print("Oprima enter para continuar con el programa", error)          
-# def menuGamas():
-#       print()
-# def menuOficina():
-#       print()
-# def menuPagos():
-#       print()
-# def menuPedido():
-#       print()
-# def menuProducto():
-#     while True:
-#         print('''
+''')
+        try:
+                opciones = int(input("Ingrese el numero de la opcion deceada: "))
+                if opciones == 0:
+                        break
+                elif opciones == 1:
+                        empleado.menu()
+                elif opciones == 2:
+                        PEmp.menu()
+                elif opciones == 3:
+                      exit()
+        except ValueError as error:
+              input("Oprima enter para continuar con el programa")
+              print(error)
+              
+        except KeyboardInterrupt as error:
+              input("Oprima enter para continuar con el programa")
+              print(error)          
+def menuGamas():
+      while True:
+            os.system("clear")
+            print('''
+
+Bienvenidos al menu de gamas
+                  0. atras
+                  1. Reportes de las gamas
+                  2. Guardar alguna gama
+                  3.Cerrar el Programa''')
+            try:
+                opciones = int(input("Ingrese el numero de la opcion deceada: "))
+                if opciones == 0:
+                        break
+                elif opciones == 1:
+                        gama.menu()
+                elif opciones == 2:
+                        pstGam.menu()
+                elif opciones == 3:
+                      exit()
+            except ValueError as error:
+              input("Oprima enter para continuar con el programa")
+              print(error)
+              
+            except KeyboardInterrupt as error:
+              input("Oprima enter para continuar con el programa")
+              print(error)          
+def menuOficina():
+      while True:
+            os.system("clear")
+            print('''
+bienvenidos al menu de oficina
+                  0.Atras
+                  1.Reportes de las gamas
+                  2.Guardar alguna gama
+                  3.Cerrar el programa''')
+            try:
+                opciones = int(input("Ingrese el numero de la opcion deceada: "))
+                if opciones == 0:
+                        break
+                elif opciones == 1:
+                        oficina.menu()
+                elif opciones == 2:
+                        pstOfi.menu()
+                elif opciones == 3:
+                      exit()
+            except ValueError as error:
+              input("Oprima enter para continuar con el programa")
+              print(error)
+              
+            except KeyboardInterrupt as error:
+              input("Oprima enter para continuar con el programa")
+              print(error)          
+def menuPagos():
+      while True:
+            os.system("clear")
+            print('''
+      bienvenido al menu de pagos
+                  0. Atras
+                  1.Reportes de los pagos
+                  2.Guardas algun pago
+                  3.Cerrar el programa''')
+            try:
+                      opciones = int(input("Ingrese el numero de la opcion deceada: "))
+                      if opciones == 0:
+                              break
+                      elif opciones == 1:
+                              pago.menu()
+                      elif opciones == 2:
+                              pstpag.menu()
+                      elif opciones == 3:
+                            exit()
+            except ValueError as error:
+                    input("Oprima enter para continuar con el programa")
+                    print(error)
+                    
+            except KeyboardInterrupt as error:
+                    input("Oprima enter para continuar con el programa")
+                    print(error)          
+def menuPedido():
+      while True:
+            os.system("clear")
+            print(''''
+                  bienvenido al menu de pedidos
+                  }
+                  0. Atras
+                  1.Reportes de los pedidos
+                  2.Guardar algun pedido
+                  3.Cerrar el programa''')
+            try:
+                      opciones = int(input("Ingrese el numero de la opcion deceada: "))
+                      if opciones == 0:
+                              break
+                      elif opciones == 1:
+                              pedido.menu()
+                      elif opciones == 2:
+                              PstPedi.menu()
+                      elif opciones == 3:
+                            exit()
+            except ValueError as error:
+                    input("Oprima enter para continuar con el programa")
+                    print(error)
+                    
+            except KeyboardInterrupt as error:
+                    input("Oprima enter para continuar con el programa")
+                    print(error)          
+def menuProducto():
+    while True:
+        print('''
                         
-#           ____  _                            _     __               __                             
-#          / __ )(_)__  ____ _   _____  ____  (_)___/ /___     ____ _/ /  ____ ___  ___  ____  __  __
-#         / __  / / _ \/ __ \ | / / _ \/ __ \/ / __  / __ \   / __ `/ /  / __ `__ \/ _ \/ __ \/ / / /
-#         / /_/ / /  __/ / / / |/ /  __/ / / / / /_/ / /_/ /  / /_/ / /  / / / / / /  __/ / / / /_/ / 
-#         /_____/_/\___/_/ /_/|___/\___/_/ /_/_/\__,_/\____/ __\__,_/_/  /_/ /_/ /_/\___/_/ /_/\__,_/  
-#         ____/ /__     ____  _________  ____/ /_  _______/ /_____  _____                            
-#         / __  / _ \   / __ \/ ___/ __ \/ __  / / / / ___/ __/ __ \/ ___/                            
-#         / /_/ /  __/  / /_/ / /  / /_/ / /_/ / /_/ / /__/ /_/ /_/ (__  )                             
-#         \__,_/\___/  / .___/_/   \____/\__,_/\__,_/\___/\__/\____/____/                              
-#                 /_/                                                                              
-#         0. Atras
-#                 1. Reportes de los productos
-#                 2. Guardar algun producto
-#         3. Cerrar programa
+          ____  _                            _     __               __                             
+         / __ )(_)__  ____ _   _____  ____  (_)___/ /___     ____ _/ /  ____ ___  ___  ____  __  __
+        / __  / / _ \/ __ \ | / / _ \/ __ \/ / __  / __ \   / __ `/ /  / __ `__ \/ _ \/ __ \/ / / /
+        / /_/ / /  __/ / / / |/ /  __/ / / / / /_/ / /_/ /  / /_/ / /  / / / / / /  __/ / / / /_/ / 
+        /_____/_/\___/_/ /_/|___/\___/_/ /_/_/\__,_/\____/ __\__,_/_/  /_/ /_/ /_/\___/_/ /_/\__,_/  
+        ____/ /__     ____  _________  ____/ /_  _______/ /_____  _____                            
+        / __  / _ \   / __ \/ ___/ __ \/ __  / / / / ___/ __/ __ \/ ___/                            
+        / /_/ /  __/  / /_/ / /  / /_/ / /_/ / /_/ / /__/ /_/ /_/ (__  )                             
+        \__,_/\___/  / .___/_/   \____/\__,_/\__,_/\___/\__/\____/____/                              
+                /_/                                                                              
+        0. Atras
+                1. Reportes de los productos
+                2. Guardar algun producto
+        3. Cerrar programa
                         
-#                         ''')
-#         try:
-#                 opcion = int(input("selecione una de las opciones: "))   
-#                 if(opcion == 1):
-#                         producto.menu()
-#                 elif(opcion == 2):
-#                         post.menu()
-#                 elif(opcion == 0):
-#                       break
-#                 elif(opcion == 3):
-#                       exit()
-#         except ValueError as error:
-#               print("Oprima enter para continuar con el programa", error)
-#         except KeyboardInterrupt as error:
-#               print("Oprima enter para continuar con el programa", error)
+                        ''')
+        try:
+                opcion = int(input("selecione una de las opciones: "))   
+                if(opcion == 1):
+                        producto.menu()
+                elif(opcion == 2):
+                        postpro.menu()
+                elif(opcion == 0):
+                      break
+                elif(opcion == 3):
+                      exit()
+        except ValueError as error:
+              input("Oprima enter para continuar con el programa")
+              print(error)
+        except KeyboardInterrupt as error:
+              input("Oprima enter para continuar con el programa")
+              print(error)
+
 
 if(__name__ == "__main__"):
-  numero = 500
-  peticion = requests.get(f"http://172.16.100.145:5000/productos?cantidad_en_stock_gte={numero}")
-  data = peticion.json()
-#   print(tabulate((data), headers="keys", tablefmt="github"))
-  print(json.dumps(data, indent=4))
-        # with open("storage/producto.json", "r") as f:
-        #         fichero = f.read()
-        #         data = json.loads(fichero)
-        # for i, val in enumerate(data):
-        #         data[i]["id"] = (i+1)
-        #         data = json.dumps(data, indent=4).encode("utf-8")
-        # with open("storage/producto.json", "wb+") as f1:
-        #         f1.write(data)
-        #         f1.close()
+  # numero = 500
+  # peticion = requests.get(f"http://172.16.100.145:5000/productos?cantidad_en_stock_gte={numero}")
+  # data = peticion.json()
+  # print(tabulate((data), headers="keys", tablefmt="github"))
+  # print(json.dumps(data, indent=4))
 
         
 
-#     while True:
-#         os.system("clear")
-#         print("""
+    while True:
+        os.system("clear")
+        print("""
           
     
-#      __  ___                               _            _             __   
-#    /  |/  /__  ____  __  __   ____  _____(_)___  _____(_)___  ____ _/ /   
-#   / /|_/ / _ \/ __ \/ / / /  / __ \/ ___/ / __ \/ ___/ / __ \/ __ `/ /    
-#  / /  / /  __/ / / / /_/ /  / /_/ / /  / / / / / /__/ / /_/ / /_/ / /     
-# /_/  /_/\___/_/ /_/\__,_/  / .___/_/  /_/_/ /_/\___/_/ .___/\__,_/_/      
-#                           /_/                       /_/                   
+     __  ___                               _            _             __   
+   /  |/  /__  ____  __  __   ____  _____(_)___  _____(_)___  ____ _/ /   
+  / /|_/ / _ \/ __ \/ / / /  / __ \/ ___/ / __ \/ ___/ / __ \/ __ `/ /    
+ / /  / /  __/ / / / /_/ /  / /_/ / /  / / / / / /__/ / /_/ / /_/ / /     
+/_/  /_/\___/_/ /_/\__,_/  / .___/_/  /_/_/ /_/\___/_/ .___/\__,_/_/      
+                          /_/                       /_/                   
                                                                       
-#                              1. Cliente
-#                              2. Oficina
-#                              3. Empleado
-#                              4. Pedidos
-#                              5. Pagos
-#                              6. Producto 
-#                              7. Cerrar el programa
-#  """)
-#         try:
-#             opcion = int(input("\nselecione una de las opciones: "))   
-#             if(opcion == 1):
-#                     cliente.menu()
-#             elif(opcion == 2):
-#                     oficina.menu()
-#             elif(opcion == 3):
-#                     empleado.menu()
-#             elif(opcion == 4):
-#                     pedido.menu()
-#             elif(opcion == 5):
-#                 pago.menu()
-#             elif(opcion == 6):
-#                 menuProducto()
-#             elif(opcion == 7):
-#                 break
+                             1. Cliente
+                             2. Oficina
+                             3. Empleado
+                             4. Pedidos
+                             5. Pagos
+                             6. Producto 
+                             7. Cerrar el programa
+ """)
+        try:
+            opcion = int(input("\nselecione una de las opciones: "))   
+            if(opcion == 1):
+                menuCliente()
+            elif(opcion == 2):
+                menuOficina()
+            elif(opcion == 3):
+                menuEmpleado()
+            elif(opcion == 4):
+                menuPedido()
+            elif(opcion == 5):
+                menuPagos()
+            elif(opcion == 6):
+                menuProducto()
+            elif(opcion == 7):
+                break
 
-#         except ValueError as error:
-#             print(error)
-#             input("Oprima enter para volver a cargar el programa")
-#         except KeyboardInterrupt as error:
-#               print(error)
-#               input("Oprima alguna tecla para continuar con el programa")
+        except ValueError as error:
+            print(error)
+            input("Oprima enter para volver a cargar el programa")
+        except KeyboardInterrupt as error:
+              print(error)
+              input("Oprima alguna tecla para continuar con el programa")
              
         
 
@@ -187,3 +315,12 @@ if(__name__ == "__main__"):
 #     print("Entrada recibida:", entrada)
 # except KeyboardInterrupt:
 #     print(tabulate(cliente.getAllClientesName()))
+          # with open("storage/producto.json", "r") as f:
+          #       fichero = f.read()
+          #       data = json.loads(fichero)
+          # for i, val in enumerate(data):
+          #       data[i]["id"] = (i+1)
+          #       data = json.dumps(data, indent=4).encode("utf-8")
+          # with open("storage/producto.json", "wb+") as f1:
+          #         f1.write(data)
+          #         f1.close()
